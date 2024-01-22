ea75699ce925bd561112c9c25cd1bf1d2eec5b8b
AlexandreCo <alexandre.github@gmail.com>
2024-01-22 13:27:46 +0100

    display graph from json

--- show_weather_station.py
+++ show_weather_station.py
@@ -8,12 +8,14 @@ import plotly.graph_objects as go
 from datetime import datetime as dt
 from datetime import timedelta
 import sqlite3
+import json
 
 sqlite_ws_file = '/home/freebox/temperature/weatherstation.sqlite'    # name of the sqlite database file
 table_ws_name = 'data'   # name of the table
 index_col = 'id'
 date_col = 'date' # name of the date column
 time_col = 'time'# name of the time column
+model_col = 'model' # name of the model column
 
 app = dash.Dash(__name__)
 app.config.update({})
@@ -25,34 +27,23 @@ todate = dt.now()
 fromdate = dt.now()-timedelta(weeks=2)
 
 
-def querywslog(fromdate,todate):
+def querywslog(model_name,col,fromdate,todate,):
     timestamp = []
     t_ws = []
-    humidity_ws = []
-    subtype_ws = []
-    battery_ok_ws = []
-    wind_dir_deg_ws = []
-    wind_avg_km_h_ws = []
-    wind_max_km_h_ws = []
     rain_mm_ws = []
+
+    print(model_name, col)
     conn = sqlite3.connect(sqlite_ws_file)
     conn.row_factory = sqlite3.Row
     c = conn.cursor()
-    c.execute("SELECT * FROM {tn} WHERE {dc} BETWEEN '{fd}' AND '{td}'".\
-        format(tn=table_ws_name, dc=date_col, fd=fromdate, td=todate))
+    c.execute("SELECT * FROM {tn} WHERE {mc} = '{ma}' AND {dc} BETWEEN '{fd}' AND '{td}'". \
+          format(tn=table_ws_name, dc=date_col, fd=fromdate, td=todate, mc=model_col, ma=model_name))
     rows = c.fetchall()
     for row in rows:
         timestamp.append(row["date"] + " " + row["time"])
-        t_ws.append(row["temperature_C"])
-        humidity_ws.append(row["humidity"])
-        subtype_ws.append(row["subtype"])
-        battery_ok_ws.append(row["battery_ok"])
-        wind_dir_deg_ws.append(row["wind_dir_deg"])
-        wind_avg_km_h_ws.append(row["wind_avg_km_h"])
-        wind_max_km_h_ws.append(row["wind_max_km_h"])
-        rain_mm_ws.append(row["rain_mm"])
+        t_ws.append(row[col])
     conn.close()
-    return {'timestamp':timestamp, 'ws_temp':t_ws, 'humidity':humidity_ws, 'subtype':subtype_ws, 'battery_ok':battery_ok_ws, 'wind_dir':wind_dir_deg_ws, 'wind_avg':wind_avg_km_h_ws, 'wind_max':wind_max_km_h_ws, 'rain':rain_mm_ws}
+    return {'timestamp':timestamp, 'ws':t_ws}
 
 def calc_rain_per_day(timestamp,rain):
     i=0
@@ -65,7 +56,7 @@ def calc_rain_per_day(timestamp,rain):
             dt_object_prev = dt.strptime(timestamp[i-1], format)
             dt_object = dt.strptime(ts, format)
             if dt_object.date() > dt_object_prev.date():
-                #print(dt_object_prev.date(), rain[i]- startrain)
+                print(dt_object_prev.date(), rain[i]- startrain)
                 datestamp.append(dt_object_prev.date())
                 if rain[i] < startrain: # we got a reset
                     startrain = rain[i]
@@ -77,49 +68,48 @@ def calc_rain_per_day(timestamp,rain):
 def create_figure_ws(figdatestart,figdateend):
     fromdate = dt.strptime(figdatestart[0:10], '%Y-%m-%d')
     todate = dt.strptime(figdateend[0:10], '%Y-%m-%d')
-    dws = querywslog(fromdate,todate)
-    rpd = calc_rain_per_day(dws['timestamp'],dws['rain'])
-    fig = make_subplots(rows=3, cols=2)
-    # add traces
-    fig.add_trace(
-        go.Scatter(x=dws['timestamp'], y=dws['ws_temp'], mode='markers+lines', name='temperature'),
-        row=1, col=1
-    )
-    fig.add_trace(
-        go.Scatter(x=dws['timestamp'], y=dws['humidity'], mode='markers', name='humidity'),
-        row=1, col=2
-    )
-    fig.add_trace(
-        go.Scatter(x=dws['timestamp'], y=dws['wind_max'], mode='markers', name='wind_max'),
-        row=2, col=1
-    )
-    fig.add_trace(
-        go.Scatter(x=dws['timestamp'], y=dws['wind_avg'], mode='markers', name='wind_avg'),
-        row=2, col=1
-    )
-    fig.add_trace(
-        go.Scatter(x=dws['timestamp'], y=dws['wind_dir'], mode='markers', name='wind_dir'),
-        row=2, col=2
-    )
-    fig.add_trace(
-        go.Scatter(x=dws['timestamp'], y=dws['rain'], mode='markers', name='rain'),
-        row=3, col=1
-    )
-    fig.add_trace(
-        go.Bar(x=rpd['datestamp'], y=rpd['rain_per_day'], text=rpd['rain_per_day'], name='rain'),
-        row=3, col=2
-    )
-    # Update yaxis properties
-    fig.update_yaxes(title_text="Temperature (C)", row=1, col=1)
-    fig.update_yaxes(title_text="humidity (%)", row=1, col=2)
-    fig.update_yaxes(title_text="speed (km/h)", row=2, col=1)
-    fig.update_yaxes(title_text="dir (deg)", row=2, col=2)
-    fig.update_yaxes(title_text="rain (mm)", row=3, col=1)
-    fig.update_yaxes(title_text="rain per day (mm)", row=3, col=2)
 
-    fig.update_layout(height=600, 
-        title_text='Weatherstation log - {:s} - {:s}'.format(fromdate.strftime("%-d %B %Y"), todate.strftime("%-d %B %Y")),
+    max_row=2
+    print(max_row)
+    fig = make_subplots(rows=max_row, cols=2)
+    nbrow=1
+
+    file1 = open('/home/freebox/rpi_rtlsdr_weather_station/display.json', 'r')
+
+    for line in file1:
+
+        d = json.loads(line)
+        model = d['model']
+        id  = d['id']
+        type = d['type']
+        src = d['src']
+        name = d['name']
+        col = d['col']
+        row = d['row']
+
+        print(model,src,col,row)
+        dws = querywslog(model,src,fromdate,todate)
+        # add traces
+
+        if(type == 1 ):
+            fig.add_trace(
+                go.Scatter(x=dws['timestamp'], y=dws['ws'], mode='markers+lines', name=model),
+                row=row, col=col
+            )
+        else:
+            rpd = calc_rain_per_day(dws['timestamp'],dws['ws'])
+            mname = 'rain ' + model
+            fig.add_trace(
+                go.Bar(x=rpd['datestamp'], y=rpd['rain_per_day'], text=rpd['rain_per_day'], name=model, marker_color='blue'),
+                row=row, col=col
+            )
+        # Update yaxis properties
+        fig.update_yaxes(title_text=name, row=row, col=col)
+
+    fig.update_layout(height=600,
+        title_text='log - {:s} - {:s}'.format(fromdate.strftime("%-d %B %Y"), todate.strftime("%-d %B %Y")),
         paper_bgcolor="LightSteelBlue")
+
     return(fig)
 
 def serve_layout():
@@ -141,7 +131,9 @@ app.layout = serve_layout
     dash.dependencies.Output('my-graph-1', 'figure'),
     [dash.dependencies.Input('date-picker-range-1', 'start_date'),
      dash.dependencies.Input('date-picker-range-1', 'end_date')])
+
 def update_output(start_date, end_date):
+
     return create_figure_ws(start_date, end_date)
 
 
